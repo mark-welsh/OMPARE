cbad7c56bfd63dc7ab941532d3277bd1e2bb4d12
Mark Welsh <welshm3@email.chop.edu>
2020-08-12 15:10:07 -0400

    Only run necessary comps. for gene exp

--- code/driver.R
+++ code/driver.R
@@ -10,76 +10,76 @@
 # sourced in order of requirement
 source('code/pubTheme.R')                       # univeral plot theme
 source('code/helper.R')                         # universal helper functions like z-score etc
-source('code/patientSampleInfo.R')              # functions to load patient info
-source('code/createCopyNumber.R')               # create a map of gene symbol and copy number
-source('code/filterDruggability.R')             # create a map of gene symbol and drug info from dgiDB
-source('code/filterCNV.R')                      # filter CNV
+#source('code/patientSampleInfo.R')              # functions to load patient info
+#source('code/createCopyNumber.R')               # create a map of gene symbol and copy number
+#source('code/filterDruggability.R')             # create a map of gene symbol and drug info from dgiDB
+#source('code/filterCNV.R')                      # filter CNV
 source('code/filterFusions.R')                  # filter Fusions
-source('code/filterMutations.R')                # filter Mutations
+#source('code/filterMutations.R')                # filter Mutations
 source("code/runRNASeqAnalysis.R")              # function to run RNA-seq analysis
 RNASeqAnalysisOut <- runRNASeqAnalysis(expData) # run in driver
 source('code/highConfidenceFindingsTable.R')    # high confidence findings
 
 # Page 1
-source('code/p1_patientSampleInfo.R')           # patient/sample information
-source('code/p1_keyClinicalFindingsTable.R')    # key findings table
-source('code/p1_diseaseSpecificInformation.R')  # disease specific information
-source('code/p1_filterGermline.R')              # filter and output germline data
-source('code/p1_genomicSummary.R')              # genomic summary table
+#source('code/p1_patientSampleInfo.R')           # patient/sample information
+#source('code/p1_keyClinicalFindingsTable.R')    # key findings table
+#source('code/p1_diseaseSpecificInformation.R')  # disease specific information
+#source('code/p1_filterGermline.R')              # filter and output germline data
+#source('code/p1_genomicSummary.R')              # genomic summary table
 
 # Page 2
 source('code/p2_plotGenes.R')                   # plot barchart of top 20 up/down genes
 source('code/p2_plotPathway.R')                 # plot barchat of top 10 up/down pathways (sorted by p-value)
 
 # Page 3
-source('code/p3_tumorSignaturePlot.R')          # plot tumor mutational signatures
-if(!is.null(tmb)){                              # plot tumor mutational burden
-  source('code/p3_tmbProfile.R')
-}
-
-# Prepare OpenPBTA and TCGA data to be used for functions in Page 4-7
-source('code/batch_correct.R')                  # batch correction function
-source('code/pbta_format.R')                    # format PBTA data
-source('code/tcga_format.R')                    # format TCGA data
-
-# Page 4 (OpenPBTA) and Page 7 (Genomically Similar PNOC008 and OpenPBTA)
-source('code/ImmuneProfile.R')                  # plot immune profile using xCell
-source('code/p4_TISProfile.R')                  # plot TIS profile
-
-# Page 5 (PNOC008 + OpenPBTA) and Page 6 (PNOC008 + TCGA GBM)
-source('code/getDimRedPlot.R')                  # plot UMAP     
-source('code/getSimilarPatients.R')             # get top 20 correlated patients
-source('code/getKMPlot.R')                      # plot KM plot of top 20 most correlated patients with survival
-
-# Page 8 (PNOC008)
-source('code/p8_tabulate_pathways.R')           # enriched pathways of PNOC008 patients from top 20 genomically similar
-
-# Page 9 (PNOC008 + OpenPBTA)
-source('code/p9_ssGSEA.R')                      # ssGSEA on top 20 genomically similar patients
-
-# Page 10 
-source('code/p10_allFindingsTable.R')           # all findings table
-
-# Page 11
-source('code/p11_plotCNV.R')
-source('code/p11_plotNetwork.R')
-source('code/p11_plotCircos.R')                 # source and run in driver
-if(exists('fusData')){
-  plotCircos(topDir = topDir)
-}
-
-# Page 12 (source and run in driver)
-source('code/p12_cnv_exp_heatmap.R')
-if(!file.exists(paste0(topDir,'/complexHeatmap_phgg.png'))){
-  create.heatmap(fname = paste0(topDir,'/complexHeatmap_phgg.png'), 
-                 genelist = genelist.heatmap$pHGG_Gene_List, 
-                 plot.layout = "h")
-}
-if(!file.exists(paste0(topDir,'/complexHeatmap_cgs.png'))){
-  create.heatmap(fname = paste0(topDir,'/complexHeatmap_cgs.png'), 
-                 genelist = genelist.heatmap$Cancer_Gene_Census_CNVs, 
-                 plot.layout = "h")
-}
-
-# Page 13 Mutational Analysis
-source('code/p13_mutational_analysis.R')
+#source('code/p3_tumorSignaturePlot.R')          # plot tumor mutational signatures
+#if(!is.null(tmb)){                              # plot tumor mutational burden
+#  source('code/p3_tmbProfile.R')
+#}
+#
+## Prepare OpenPBTA and TCGA data to be used for functions in Page 4-7
+#source('code/batch_correct.R')                  # batch correction function
+#source('code/pbta_format.R')                    # format PBTA data
+#source('code/tcga_format.R')                    # format TCGA data
+#
+## Page 4 (OpenPBTA) and Page 7 (Genomically Similar PNOC008 and OpenPBTA)
+#source('code/ImmuneProfile.R')                  # plot immune profile using xCell
+#source('code/p4_TISProfile.R')                  # plot TIS profile
+#
+## Page 5 (PNOC008 + OpenPBTA) and Page 6 (PNOC008 + TCGA GBM)
+#source('code/getDimRedPlot.R')                  # plot UMAP     
+#source('code/getSimilarPatients.R')             # get top 20 correlated patients
+#source('code/getKMPlot.R')                      # plot KM plot of top 20 most correlated patients with survival
+#
+## Page 8 (PNOC008)
+#source('code/p8_tabulate_pathways.R')           # enriched pathways of PNOC008 patients from top 20 genomically similar
+#
+## Page 9 (PNOC008 + OpenPBTA)
+#source('code/p9_ssGSEA.R')                      # ssGSEA on top 20 genomically similar patients
+#
+## Page 10 
+#source('code/p10_allFindingsTable.R')           # all findings table
+#
+## Page 11
+#source('code/p11_plotCNV.R')
+#source('code/p11_plotNetwork.R')
+#source('code/p11_plotCircos.R')                 # source and run in driver
+#if(exists('fusData')){
+#  plotCircos(topDir = topDir)
+#}
+#
+## Page 12 (source and run in driver)
+#source('code/p12_cnv_exp_heatmap.R')
+#if(!file.exists(paste0(topDir,'/complexHeatmap_phgg.png'))){
+#  create.heatmap(fname = paste0(topDir,'/complexHeatmap_phgg.png'), 
+#                 genelist = genelist.heatmap$pHGG_Gene_List, 
+#                 plot.layout = "h")
+#}
+#if(!file.exists(paste0(topDir,'/complexHeatmap_cgs.png'))){
+#  create.heatmap(fname = paste0(topDir,'/complexHeatmap_cgs.png'), 
+#                 genelist = genelist.heatmap$Cancer_Gene_Census_CNVs, 
+#                 plot.layout = "h")
+#}
+#
+## Page 13 Mutational Analysis
+#source('code/p13_mutational_analysis.R')
--- code/readData.R
+++ code/readData.R
@@ -4,7 +4,6 @@
 #############################
 
 source('code/load_reference.R') # load reference data
-source('code/filterFusions.R')  # load filter fusions script to filter fusions
 
 readData <- function(topDir, fusion_method = c("star","arriba"), snv_pattern = "all"){
   
@@ -14,72 +13,6 @@ readData <- function(topDir, fusion_method = c("star","arriba"), snv_pattern = "
   if(length(sampleInfo) == 1){
     sampleInfo <- read.delim(sampleInfo, stringsAsFactors = F)
     assign("sampleInfo", sampleInfo, envir = globalenv())
-  }
-  
-  # mutation data (can be multiple with .maf extension)
-  # if snv_pattern is all, then use all files except consensus
-  somatic.mut.pattern <- '*.maf'
-  mutFiles <- list.files(path = topDir, pattern = somatic.mut.pattern, recursive = TRUE, full.names = T)
-  if(snv_pattern == "all"){ 
-    mutFiles <- grep('consensus', mutFiles, invert = TRUE, value = TRUE)
-  } else {
-    mutFiles <- grep(snv_pattern, mutFiles, value = TRUE)
-  }
-  if(length(mutFiles) >= 1){
-    mutFiles <- lapply(mutFiles, data.table::fread, skip = 1, stringsAsFactors = F)
-    mutData <- data.table::rbindlist(mutFiles)
-    mutData <- as.data.frame(mutData)
-    mutData <- unique(mutData)
-    assign("mutData", mutData, envir = globalenv())
-  } 
-  
-  # germline data
-  mutFiles <- list.files(path = topDir, pattern = 'hg38_multianno.txt.gz', recursive = TRUE, full.names = T)
-  if(length(mutFiles) >= 1){
-    mutFiles <- lapply(mutFiles, data.table::fread, stringsAsFactors = F)
-    mutData.germ <- data.table::rbindlist(mutFiles)
-    mutData.germ <- as.data.frame(mutData.germ)
-    mutData.germ <- unique(mutData.germ)
-    assign("mutData.germ", mutData.germ, envir = globalenv())
-  } 
-  
-  # copy number pvalue file
-  cnvData <- list.files(path = topDir, pattern = "*.CNVs.p.value.txt", recursive = TRUE, full.names = T)
-  if(length(cnvData) == 1){
-    cnvData <- data.table::fread(cnvData, header = T, check.names = T)
-    cnvData <- cnvData %>% 
-      dplyr::select(chr, start, end, copy.number, 
-                    status, WilcoxonRankSumTestPvalue) %>%
-      as.data.frame()
-    assign("cnvData", cnvData, envir = globalenv())
-  }
-  
-  # copy number ratio data for plot
-  cnvRatioData <- list.files(path = topDir, pattern = "*controlfreec.ratio.txt$", recursive = TRUE, full.names = T)
-  if(length(cnvRatioData) == 1){
-    cnvRatioData <- data.table::fread(cnvRatioData, header = T)
-    cnvRatioData <- as.data.frame(cnvRatioData)
-    assign("cnvRatioData", cnvRatioData, envir = globalenv())
-  }
-  
-  # fusion data (chose either star or arriba or both)
-  # fusion_method determines the pattern to be searched
-  if(fusion_method == "star"){
-    fusPattern = "*star-fusion.fusion_candidates.final"
-  } else if(fusion_method == "arriba") {
-    fusPattern = "*.arriba.fusions.tsv"
-  } else {
-    fusPattern = "*star-fusion.fusion_candidates.final|*.arriba.fusions.tsv"
-  }
-  # read fusion files + filter and merge them
-  fusFiles <- list.files(path = topDir, pattern = fusPattern, recursive = TRUE, full.names = T)
-  if(length(fusFiles) >= 1){
-    fusFiles <- lapply(fusFiles, filterFusions)
-    fusData <- do.call('rbind', fusFiles)
-    fusData <- unique(fusData)
-    if(nrow(fusData) >= 1){
-      assign("fusData", fusData, envir = globalenv())
-    }
   }
   
   # expression data: TPM (can be only 1 per patient with .genes.results)
